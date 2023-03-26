70a5e01aaa049946c858a6bb486a5fe8946787d5
SAI TEJA H R <98202897+Saitejahr@users.noreply.github.com>
2023-03-26 14:53:39 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
 var isDate = function (input) {
-  //   write your code here
+  if(input === new Date()){
+	  return true;
+  }else{
+	  return false;
+  }
 };
 
 // Do not change the code below.
-const input = prompt("Enter Date.");
+ const input = prompt("Enter Date.");
 alert(isDate(input));
